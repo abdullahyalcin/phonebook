b7a3a764475ac5c3e484850e449f18cfb56d0704
abdullahyalcin <apoyalcn98@gmail.com>
2023-12-21 21:41:41 +0100

    update

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "abdullahyalcin"
+}
+
+variable "git-token" {
+  default = "y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"
+}
+
+variable "key-name" {
+  default = "mackey"
+}
+
+variable "hosted-zone" {
+  default = "abdullahyalcin.com"
+}
